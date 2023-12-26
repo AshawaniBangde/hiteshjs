247f26386ed1749d580fa207bbb254d4258c3e63
geeta <107531267+AshawaniBangde@users.noreply.github.com>
2023-12-26 17:33:50 +0000

    dev config donee

---
+++ basic/var.js
@@ -0,0 +1,10 @@
+// varible and const
+
+const accountId = 144322;
+let accountEmail = "ss@gmail.com";
+var aacountPassword = "12345"
+accountCity ="jaipur"
+
+// accountId = 2;
+
+console.log(accountId);
